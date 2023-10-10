1bc0c0a359eb63116fe8d1e7e220c8441659adec
Jan Steikowski <j.steikowski@aol.com>
2023-10-10 12:04:53 +0700

    Start with Preview style

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,3 +1,21 @@
+import { ReactElement } from "react";
+
+function InfoResult(): ReactElement {
+  return (
+    <div className="h-1/5 flex justify-between">
+      <div className="flex flex-col gap-2 items-start">
+        <h2>Hallo</h2>
+        <h3>Arbeiter</h3>
+      </div>
+    </div>
+  );
+}
+
 export default function Preview() {
-  return <div className="w-10 h-10 bg-blue-100"></div>;
+  return (
+    <div id="preview" className="flex-grow bg-white relative text-black">
+      <InfoResult></InfoResult>
+      <hr className="h-1 bg-black"></hr>
+    </div>
+  );
 }

5e81bff0f18466e4e6ea5d869bdc1d9e322a1db4
Jan Steikowski <j.steikowski@aol.com>
2023-10-10 12:04:40 +0700

    Delete App.css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
 import EducationForm from "./components/Education";
 import GeneralInfo from "./components/GeneralInformation";
 import PracticalForm from "./components/Practical";
 import Preview from "./components/Preview";
 
 function App() {
+
   return (
     <div className="flex flex-col lg:flex-row gap-4">
       <div className="flex flex-col gap-4 flex-grow">

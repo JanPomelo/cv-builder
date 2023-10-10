94a34ac5b4dc745f211bdb2c0a676615b35c21c0
Jan Steikowski <j.steikowski@aol.com>
2023-10-10 11:15:55 +0700

    Correct spelling mistake in generalInfo

--- src/components/GeneralInformation.tsx
+++ src/components/GeneralInformation.tsx
@@ -3,7 +3,7 @@ import InputDiv from "./inputDiv"
 export default function GeneralInfo() {
   return (
     <form className=" bg-blue-100 text-black">
-      <h2>Personal Information: </h2>
+      <h2>Personal Information</h2>
       <InputDiv name='First Name' id='fName'></InputDiv>
       <InputDiv name='Last Name' id='lName'></InputDiv>
       <InputDiv name='Description' id='description'></InputDiv>

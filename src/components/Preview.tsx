c5dd7f41f2df3aaf944001efcc67ac722e765f50
Jan Steikowski <j.steikowski@aol.com>
2023-10-12 10:44:13 +0700

    change preview style to be more in line with print

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -13,9 +13,9 @@ function InfoResult({
 }): ReactElement {
   return (
     <div className="h-1/6 flex justify-between">
-      <div className="flex flex-col items-start w-3/8 previewTitle">
+      <div className="flex flex-col items-start justify-around w-3/8 previewTitle">
         <div className="font-bold previewName">{name}</div>
-        <div className="ml-3 font-bold previewOccupation">{profession}</div>
+        <div className="previewOccupation">{profession}</div>
         <div className="flex items-center">
           <span id="locationSpan"></span>
           <h3 className="">{location}</h3>
@@ -38,7 +38,7 @@ export default function Preview({
   image: { imageURL: string; imageName: string };
 }) {
   return (
-    <div id="preview" className="flex-grow bg-white relative text-black">
+    <div id="preview" className="bg-white relative text-black">
       <InfoResult name={name} location={location} profession={profession} image={image} />
       <hr className="h-1 bg-black"></hr>
     </div>

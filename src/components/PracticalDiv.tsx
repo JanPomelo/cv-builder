2a598d095916a73c078343b4d285696563525a80
Jan Steikowski <j.steikowski@aol.com>
2023-10-12 18:43:40 +0700

    Add id and keys to the jobs and list

--- src/components/PracticalDiv.tsx
+++ src/components/PracticalDiv.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, MouseEventHandler, useState } from "react";
 import { profExp } from "../types";
 import PracticalForm from "./PracticalForm";
+import { v4 as uuidv4 } from "uuid";
 function AddButton({ id, onClick }: { id: string; onClick: MouseEventHandler<HTMLButtonElement> }) {
   return (
     <button className="flex items-center gap-1" id={id} onClick={onClick}>
@@ -54,6 +55,7 @@ export default function PracticalDiv() {
       const startDate = adjustDateFormat(form.startDateJob.value);
       const endDate = adjustDateFormat(form.endDateJob.value);
       const newJob: profExp = {
+        id: uuidv4(),
         company: form.company.value,
         jobTitle: form.jobTitle.value,
         description: form.jobDescription.value,
@@ -79,7 +81,7 @@ export default function PracticalDiv() {
       </div>
       {jobs.map((job) => {
         return (
-          <div className="flex flex-col text-white items-start bg-my-bg rounded-lg px-2 py-0.5 mb-2">
+          <div className="flex flex-col text-white items-start bg-my-bg rounded-lg px-2 py-0.5 mb-2" key={job.id}>
             <h3>
               <b>{job.jobTitle}</b> at {job.company}
             </h3>
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
 export type changeHandler = (e: React.FormEvent<HTMLInputElement>) => void;
 
-export type profExp = { company: string, jobTitle: string, location: string, description: string, startDate: string, endDate: string };
+export type profExp = {id:string, company: string, jobTitle: string, location: string, description: string, startDate: string, endDate: string };

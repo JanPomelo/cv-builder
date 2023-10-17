5a052cc2e5a7c0d22461380d36be134bdad4cb1b
Jan Steikowski <j.steikowski@aol.com>
2023-10-17 15:39:37 +0700

    Adjust heading style so the text always stays left

--- src/components/GroupDivHeading.tsx
+++ src/components/GroupDivHeading.tsx
@@ -2,7 +2,6 @@ function handleOnClick(e: React.MouseEvent<HTMLButtonElement>) {
   const img = e.target as HTMLImageElement;
   const button = img.parentElement as HTMLButtonElement;
   const contentDiv = button.parentElement?.nextSibling as HTMLDivElement;
-  console.log(contentDiv);
   button.classList.toggle("expandButIn");
   button.classList.toggle("expandButOut");
   contentDiv.classList.toggle('closed');
@@ -14,7 +13,7 @@ export default function GroupDivHeading({ name, spanID }: { name: string; spanID
     <div className="flex justify-between items-center">
       <div className="flex gap-2 items-center mb-2">
         <span id={spanID}></span>
-        <h2 className="text-xl font-bold">{name}</h2>
+        <h2 className="text-xl text-left font-bold">{name}</h2>
       </div>
       <button className="expandButIn" onClick={handleOnClick}>
         <img src="../src/assets/arrow.svg" alt="arrow" />

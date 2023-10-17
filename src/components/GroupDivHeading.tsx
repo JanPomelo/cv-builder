4c194557c3a7e865761fc643c015b257ad9155cf
Jan Steikowski <j.steikowski@aol.com>
2023-10-17 15:55:05 +0700

    Fix wrong focus behaviour when divs are closed

--- src/components/GroupDivHeading.tsx
+++ src/components/GroupDivHeading.tsx
@@ -1,22 +1,21 @@
-function handleOnClick(e: React.MouseEvent<HTMLButtonElement>) {
-  const img = e.target as HTMLImageElement;
-  const button = img.parentElement as HTMLButtonElement;
+function handleOnClick(e: React.MouseEvent<HTMLButtonElement> ) {
+  const button = e.target as HTMLButtonElement;
   const contentDiv = button.parentElement?.nextSibling as HTMLDivElement;
   button.classList.toggle("expandButIn");
   button.classList.toggle("expandButOut");
-  contentDiv.classList.toggle('closed');
-  contentDiv.classList.toggle('expanded');
+  contentDiv.classList.toggle("closed");
+  contentDiv.classList.toggle("expanded");
 }
 
 export default function GroupDivHeading({ name, spanID }: { name: string; spanID: string }) {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center min-h-fit">
       <div className="flex gap-2 items-center mb-2">
         <span id={spanID}></span>
         <h2 className="text-xl text-left font-bold">{name}</h2>
       </div>
       <button className="expandButIn" onClick={handleOnClick}>
-        <img src="../src/assets/arrow.svg" alt="arrow" />
+        <img src="../src/assets/arrow.svg" alt="arrow" className="arrow"/>
       </button>
     </div>
   );
